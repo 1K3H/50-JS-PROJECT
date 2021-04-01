e237ba8f4163ded7c27e33a4af4213dde92bb30e
김승환 <dkadowlwh@naver.com>
2021-04-01 23:28:03 +0900

    Add: 불러운 data 형식에 맞춰서 DOM에 삽입하기

--- Github Profiles/script.js
+++ Github Profiles/script.js
@@ -2,15 +2,52 @@ const APIURL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
 
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
 
-    console.log(data);
-  } catch(err) {
-    console.log(error);
+    createUserCard(data);
+  } catch (err) {
+    if(err.response.status === 404) {
+      createErrorCard('No profile with this username');
+    }
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = `
+  <div class="card">
+      <div>
+        <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+      </div>
+      <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio ? user.bio : ''}</p>
+
+        <ul>
+          <li>${user.followers} <strong>Followers</strong></li>
+          <li>${user.following} <strong>Following</strong></li>
+          <li>${user.public_repos} <strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
@@ -18,9 +55,9 @@ form.addEventListener('submit', (e) => {
 
   const user = search.value;
 
-  if(user) {
+  if (user) {
     getUser(user);
-    
+
     search.value = '';
   }
 });
