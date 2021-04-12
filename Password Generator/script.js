8ac77d79804c13092f5b1655194c2a85b4a178b1
김승환 <dkadowlwh@naver.com>
2021-04-12 21:06:49 +0900

    Add: 체크된 요소 이용해서 비밀번호 생성하기

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,8 +1,48 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateBtn = document.getElementById('generate');
+const clipboardBtn = document.getElementById('clipboard');
+
 const randomFunc = {
-  lower : getRandomLower,
   upper : getRandomUpper,
+  lower : getRandomLower,
   number : getRandomNumber,
-  Symbol : getRandomSymbol
+  symbol : getRandomSymbol
+}
+
+generateBtn.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasUpper = uppercaseEl.checked;
+  const hasLower = lowercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(length, hasUpper, hasLower, hasNumber, hasSymbol);
+});
+
+function generatePassword(length, upper, lower, number, symbol) {
+  let generatedPassword = '';
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+  if(typesCount === 0) {
+    return '';
+  }
+
+  for(let i = 0; i < length; i+=typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
 }
 
 function getRandomLower() {
